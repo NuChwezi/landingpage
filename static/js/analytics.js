832a2b3ff43f936e9383cf4341ab089aed21eb79
Ow. Nemesis Fixx <joewillrich@gmail.com>
2020-11-06 19:53:58 +0300

    apply a multiplier on the ga target score metrics

--- static/js/analytics.js
+++ static/js/analytics.js
@@ -1,7 +1,8 @@
+var BASE_VALUE_MULTIPLIER = 4;
 window.__stats = function stats(){
     $('a').click(function(){
         var track_id = $(this).data('ti');
-        var score = $(this).data('sc');
+        var score = $(this).data('sc') * BASE_VALUE_MULTIPLIER;
         ga('send', 'event', 'links', 'click', track_id, score || 1);
     });
 }
